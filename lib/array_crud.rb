73e3cd4ae385e38129e66bad00a26b24737a6a9c
Tomas Tobar <codetombomb@gmail.com>
2019-11-29 21:28:42 +0000

    Done.

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -1,39 +1,78 @@
+#Pass
+
 def create_an_empty_array
-  
+  []
 end
+#--------------------------------------------------------
+
+
+#Pass
 
 def create_an_array
-  
+  ["one", "two", "three", "four"]
 end
+#--------------------------------------------------------
+
+
+#Pass
 
 def add_element_to_end_of_array(array, element)
-  
+  return array, element
 end
 
+add_element_to_end_of_array(num_array = ["one", "two", "three", "four"], num_array << "five" )
+#---------------------------------------------------------
+
+#Pass
 def add_element_to_start_of_array(array, element)
-  
+  return array.unshift(element)
 end
+add_element_to_start_of_array([num_array = "one", "two", "three", "four"], "zero")
 
+#---------------------------------------------------------
+
+#Pass
 def remove_element_from_end_of_array(array)
-  
-end
+  return array.pop
+  end
+remove_element_from_end_of_array([num_array = "one", "two", "three", "four"])
 
+#------------------------------------------------------------------
+
+#Pass
 def remove_element_from_start_of_array(array)
-  
+  return array.shift
 end
+remove_element_from_start_of_array([num_array = "one", "two", "three", "four"])
+#-------------------------------------------------------------------
 
+#Pass
 def retrieve_element_from_index(array, index_number)
+  return array[index_number]
+  end
+  retrieve_element_from_index([num_array = "one", "two", "three", "four"],0)
   
-end
+#-------------------------------------------------------------------
 
+#Pass
 def retrieve_first_element_from_array(array)
-  
+ return array[0]
 end
+retrieve_first_element_from_array([num_array = "one", "two", "three", "four"])
 
+#------------------------------------------------------------------
+
+#Pass
 def retrieve_last_element_from_array(array)
-  
+  return array[-1]
 end
+retrieve_last_element_from_array([num_array = "one", "two", "three", "four"])
 
+#------------------------------------------------------------------
+
+#Pass
 def update_element_from_index(array, index_number, element)
-
+  return array[index_number] = element
 end
+
+update_element_from_index([num_array = "one", "two", "tree", "four"],2,"three" )
